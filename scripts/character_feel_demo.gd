b33125ed491f623dc63ba5a3623aa65bfece34fc
leo <36908896+ancientboy@users.noreply.github.com>
2026-07-29 04:12:51 +0800

    Restore character demo to 26e9f58 baseline

--- scripts/character_feel_demo.gd
+++ scripts/character_feel_demo.gd
@@ -1,30 +1,15 @@
 extends Node2D
 
-const UI_FONT: Font = preload("res://assets/fonts/DreadboundChineseFull.otf")
-
 @onready var _player := $Player as Player
-@onready var _martial_artist_trial := (
-	$Player/MartialArtistTrialCharacter as MartialArtistTrialCharacter
-)
-@onready var _humanoid_actions := (
-	$Player/UniversalHumanoidActionCharacter as UniversalHumanoidActionCharacter
-)
 @onready var _hud_panel := $HUD/Panel as PanelContainer
 @onready var _touch_test_buttons := $HUD/TouchTestButtons as HBoxContainer
-@onready var _trial_button := $HUD/TouchTestButtons/Trial as Button
 
 
 func _ready() -> void:
-	var demo_theme := Theme.new()
-	demo_theme.default_font = UI_FONT
-	_hud_panel.theme = demo_theme
-	_touch_test_buttons.theme = demo_theme
 	$HUD/TouchTestButtons/Hit.pressed.connect(_trigger_hit)
 	$HUD/TouchTestButtons/Death.pressed.connect(_trigger_death)
 	$HUD/TouchTestButtons/Reset.pressed.connect(_reset_demo)
-	_trial_button.pressed.connect(_toggle_character_trial)
 	get_viewport().size_changed.connect(_layout_touch_ui)
-	_update_trial_button()
 	_layout_touch_ui()
 
 
@@ -39,8 +24,6 @@ func _unhandled_key_input(event: InputEvent) -> void:
 			_trigger_hit()
 		KEY_K:
 			_trigger_death()
-		KEY_V:
-			_toggle_character_trial()
 
 
 func _trigger_hit() -> void:
@@ -59,28 +42,12 @@ func _reset_demo() -> void:
 	get_tree().reload_current_scene()
 
 
-func _toggle_character_trial() -> void:
-	_humanoid_actions.set_action_library_enabled(
-		not _humanoid_actions.is_action_library_enabled()
-	)
-	_update_trial_button()
-
-
-func _update_trial_button() -> void:
-	_trial_button.text = (
-		"返回正式武斗师"
-		if _humanoid_actions.is_action_library_enabled()
-		else "查看骨骼调试"
-	)
-
-
 func _layout_touch_ui() -> void:
 	var viewport_size := get_viewport_rect().size
 	var narrow := viewport_size.x < 900.0
-	var compact := viewport_size.x < 1400.0
 	_hud_panel.offset_right = minf(790.0, viewport_size.x - 24.0)
-	_hud_panel.offset_bottom = 444.0 if narrow else 430.0
-	_touch_test_buttons.offset_left = maxf(24.0, viewport_size.x - 568.0)
+	_hud_panel.offset_bottom = 314.0 if narrow else 332.0
+	_touch_test_buttons.offset_left = maxf(24.0, viewport_size.x - 424.0)
 	_touch_test_buttons.offset_right = viewport_size.x - 28.0
-	_touch_test_buttons.offset_top = 456.0 if narrow else (442.0 if compact else 28.0)
+	_touch_test_buttons.offset_top = 28.0 if not narrow else 326.0
 	_touch_test_buttons.offset_bottom = _touch_test_buttons.offset_top + 60.0
